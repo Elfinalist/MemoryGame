440430c0c37f7314996edfff17c3820159640c3f
Kingsley <elfinalist@gmail.com>
2022-07-23 15:04:07 +0300

    upload images

---
+++ app.js
@@ -0,0 +1,53 @@
+  const cardArray = [
+    {
+        name: 'ayden',
+        img: 'images/ayden.jpg',
+    },
+    {
+        name: 'Bunke',
+        img: 'images/Bunke.jpeg',
+    },
+    {
+        name: 'dadioo',
+        img: 'images/dadioo2.jpeg',
+    },
+    {
+        name: 'reign',
+        img: 'images/reign1.jpg',
+    },
+    {
+        name: 'rhoda',
+        img: 'images/rhoda.jpeg',
+    },
+    {
+        name: 'momaa2',
+        img: 'images/momaa2.jpeg',
+    },
+    {
+        name: 'ayden',
+        img: 'images/ayden.jpg',
+    },
+    {
+        name: 'Bunke',
+        img: 'images/Bunke.jpeg',
+    },
+    {
+        name: 'dadioo',
+        img: 'images/dadioo2.jpeg',
+    },
+    {
+        name: 'reign',
+        img: 'images/reign1.jpg',
+    },
+    {
+        name: 'rhoda',
+        img: 'images/rhoda.jpeg',
+    },
+    {
+        name: 'momaa2',
+        img: 'images/momaa2.jpeg',
+    }
+  ] 
+
+
+cardArray.sort(() => 0.5 - Math.random()) //fo sorting an array randomly
